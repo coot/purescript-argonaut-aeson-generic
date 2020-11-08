cd471835b9c0b661652f4b7dc2bddad27cdf6484
Ignat Insarov <kindaro@gmail.com>
2020-11-08 22:05:15 +0500

    Fix decoding.

--- src/Data/Argonaut/Aeson/Decode/Generic.purs
+++ src/Data/Argonaut/Aeson/Decode/Generic.purs
@@ -6,11 +6,12 @@ module Data.Argonaut.Aeson.Decode.Generic
   , genericDecodeAeson
   ) where
 
-import Prelude
+import Prelude (bind, const, discard, map, pure, show, when, ($), (/=), (<$>), (<<<), (<>), (==))
 
 import Control.Alt ((<|>))
+import Data.Argonaut.Aeson.Helpers (class AreAllConstructorsNullary, class IsSingleConstructor, Mode(..), areAllConstructorsNullary, isSingleConstructor)
 import Data.Argonaut.Aeson.Options (Options(Options), SumEncoding(..))
-import Data.Argonaut.Core (Json, caseJson, fromBoolean, fromNumber, fromObject, fromString, jsonNull, toObject, toString)
+import Data.Argonaut.Core (Json, caseJson, caseJsonArray, caseJsonString, fromBoolean, fromNumber, fromObject, fromString, jsonNull, toObject, toString)
 import Data.Argonaut.Decode.Generic.Rep (class DecodeRepArgs, decodeRepArgs)
 import Data.Array (singleton)
 import Data.Bifunctor (lmap)
@@ -19,6 +20,7 @@ import Data.Generic.Rep as Rep
 import Data.Maybe (Maybe(..))
 import Foreign.Object as SM
 import Data.Symbol (class IsSymbol, SProxy(..), reflectSymbol)
+import Type.Proxy (Proxy(..))
 
 class DecodeAeson r where
   decodeAeson :: Options -> Json -> Either String r
@@ -26,27 +28,41 @@ class DecodeAeson r where
 instance decodeAesonNoConstructors :: DecodeAeson Rep.NoConstructors where
   decodeAeson _ _ = Left "Cannot decode empty data type"
 
-instance decodeAesonSum :: DecodeAeson' (Rep.Sum a b) => DecodeAeson (Rep.Sum a b) where
-  decodeAeson = decodeAeson'
+instance decodeAesonConstructor
+  :: ( DecodeRepArgs a
+     , IsSymbol name
+     , AreAllConstructorsNullary (Rep.Constructor name a)
+     , IsSingleConstructor (Rep.Constructor name a)
+     )
+  => DecodeAeson (Rep.Constructor name a) where
+  decodeAeson o thing = decodeAeson' mode o thing
+    where
+      mode = Mode
+        { _Mode_ConstructorIsSingle: isSingleConstructor (Proxy :: Proxy (Rep.Constructor name a))
+        , _Mode_ConstructorsAreAllNullary: areAllConstructorsNullary (Proxy :: Proxy (Rep.Constructor name a))
+        }
 
-instance decodeAesonConstructor :: (IsSymbol name, DecodeRepArgs a) => DecodeAeson (Rep.Constructor name a) where
-  decodeAeson (Options options) j =
-    if options.tagSingleConstructors
-    then decodeAeson' (Options options) j
-    else do
-      let name = reflectSymbol (SProxy :: SProxy name)
-      let decodingErr msg = "When decoding a " <> name <> ": " <> msg
-      {init, rest} <- let values = toJsonArray j in lmap decodingErr $ decodeRepArgs values
-      pure $ Rep.Constructor init
+instance decodeAesonSum
+  :: ( DecodeAeson' (Rep.Sum a b)
+     , AreAllConstructorsNullary (Rep.Sum a b)
+     , IsSingleConstructor (Rep.Sum a b)
+     )
+  => DecodeAeson (Rep.Sum a b) where
+  decodeAeson o thing = decodeAeson' mode o thing
+    where
+      mode = Mode
+        { _Mode_ConstructorIsSingle: isSingleConstructor (Proxy :: Proxy (Rep.Sum a b))
+        , _Mode_ConstructorsAreAllNullary: areAllConstructorsNullary (Proxy :: Proxy (Rep.Sum a b))
+        }
 
 class DecodeAeson' r where
-  decodeAeson' :: Options -> Json -> Either String r
+  decodeAeson' :: Mode -> Options -> Json -> Either String r
 
 instance decodeAesonNoConstructors' :: DecodeAeson' Rep.NoConstructors where
-  decodeAeson' _ _ = Left "Cannot decode empty data type"
+  decodeAeson' _ _ _ = Left "Cannot decode empty data type"
 
 instance decodeAesonSum' :: (DecodeAeson' a, DecodeAeson' b) => DecodeAeson' (Rep.Sum a b) where
-  decodeAeson' o j = Rep.Inl <$> decodeAeson' o j <|> Rep.Inr <$> decodeAeson' o j
+  decodeAeson' mode o j = Rep.Inl <$> decodeAeson' mode o j <|> Rep.Inr <$> decodeAeson' mode o j
 
 toJsonArray :: Json -> Array Json
 toJsonArray = caseJson
@@ -58,22 +74,51 @@ toJsonArray = caseJson
   (singleton <<< fromObject)
 
 instance decodeAesonConstructor' :: (IsSymbol name, DecodeRepArgs a) => DecodeAeson' (Rep.Constructor name a) where
-  decodeAeson' (Options { sumEncoding: TaggedObject r }) j = do
+  decodeAeson' mode options json =
     let name = reflectSymbol (SProxy :: SProxy name)
-    let decodingErr msg = "When decoding a " <> name <> ": " <> msg
-    jObj <- note (decodingErr "expected an object") (toObject j)
-    jTag <- note (decodingErr (show r.tagFieldName <> " property is missing")) (SM.lookup r.tagFieldName jObj)
-    tag <- note (decodingErr (show r.tagFieldName <> " property is not a string")) (toString jTag)
-    when (tag /= name) $
-      Left $ decodingErr "'tag' property has an incorrect value"
-    {init, rest} <- case SM.lookup r.contentsFieldName jObj of
-      Just jValue ->
-        let values = toJsonArray jValue
-        in lmap decodingErr $ decodeRepArgs values
-      Nothing -> do
-        let jObj' = SM.delete r.tagFieldName jObj
-        lmap decodingErr $ decodeRepArgs $ singleton (fromObject jObj')
-    pure $ Rep.Constructor init
+        decodingErr msg = "When decoding a " <> name <> ": " <> msg
+    in case {mode: mode, options: options} of
+
+        { mode: Mode {_Mode_ConstructorIsSingle: true, _Mode_ConstructorsAreAllNullary: true}
+        , options: Options {tagSingleConstructors: false, allNullaryToStringTag: true}
+        } -> case caseJsonArray Nothing (Just <<< decodeRepArgs) json of
+          Nothing -> Left (decodingErr ("Expected an empty array!"))
+          Just (Left errorMessage) -> Left (decodingErr errorMessage)
+          Just (Right {init: x, rest: _}) -> Right (Rep.Constructor x)
+            -- We know that `x ≡ Rep.NoArguments`,
+            -- but we cannot supply the value directly because the method is polymorphic.
+
+        { mode: Mode {_Mode_ConstructorsAreAllNullary: true}
+        , options: Options {allNullaryToStringTag: true}
+        } -> case caseJsonString Nothing Just json of
+          Nothing -> Left (decodingErr ("Expected a string!"))
+          Just tag -> if tag == name
+            then map (Rep.Constructor <<< (_.init)) (decodeRepArgs [ ])
+              -- We know that it should be `Rep.Constructor Rep.NoArguments`,
+              -- but we cannot supply the value directly because the method is polymorphic.
+            else Left "Mismatched constructor tag!"
+
+        { mode: Mode {_Mode_ConstructorIsSingle: true}
+        , options: Options {tagSingleConstructors: false}
+        } -> do
+            {init, rest} <- let values = toJsonArray json in lmap decodingErr $ decodeRepArgs values
+            pure $ Rep.Constructor init
+
+        { options: Options {sumEncoding: TaggedObject taggedObject}
+        } -> do
+          jObj <- note (decodingErr "expected an object") (toObject json)
+          jTag <- note (decodingErr (show taggedObject.tagFieldName <> " property is missing")) (SM.lookup taggedObject.tagFieldName jObj)
+          tag <- note (decodingErr (show taggedObject.tagFieldName <> " property is not a string")) (toString jTag)
+          when (tag /= name) $
+            Left $ decodingErr "'tag' property has an incorrect value"
+          {init, rest} <- case SM.lookup taggedObject.contentsFieldName jObj of
+            Just jValue ->
+              let values = toJsonArray jValue
+              in lmap decodingErr $ decodeRepArgs values
+            Nothing -> do
+              let jObj' = SM.delete taggedObject.tagFieldName jObj
+              lmap decodingErr $ decodeRepArgs $ singleton (fromObject jObj')
+          pure $ Rep.Constructor init
 
 -- | Decode `Json` Aeson representation of a value which has a `Generic` type.
 genericDecodeAeson :: forall a r. Rep.Generic a r => DecodeAeson r => Options -> Json -> Either String a
